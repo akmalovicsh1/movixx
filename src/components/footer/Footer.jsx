6e5fd4495ec26e27089fb189b5cba1d3561c68a7
akmalovicsh1 <129235465+akmalovicsh1@users.noreply.github.com>
2023-07-31 11:24:50 +0500

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                   
+                </ul>
+                <div className="infoText">
+                    This is website by : Ro'ziqulov Shahzod :)        This website is intended to see Movie Traders
+
+                 
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
